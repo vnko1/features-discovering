d2120b9756b78755e3f5185e20209625fee27d3e
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-07-28 09:32:42 +0300

    save updates

--- src/app/fetch/page.tsx
+++ src/app/fetch/page.tsx
@@ -1,15 +1,68 @@
 "use client";
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
 function Page() {
+  const [title, setTitle] = useState("");
+  const [image, setImage] = useState<null | File>(null);
+
   const handleClick = async () => {
     const res = await fetch("/api");
     const data = await res.json();
-    console.log("🚀 ~ handleClick ~ res:", data);
+    console.log("🚀 ~ handleClick ~ res:", data.data.data);
+  };
+
+  const handleGetImages = async () => {
+    const res = await fetch("http://localhost:1337/api/upload/files");
+    const data = await res.json();
+    console.log("🚀 ~ handleGetImages ~ data:", data);
+  };
+
+  const handleUploadedFile = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files?.length) return;
+    const filesList = event.target.files;
+
+    const [image] = Array.from(filesList);
+
+    setImage(image);
+
+    // event.target.value = "";
+  };
+
+  const onHandleTitleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTitle(event.target.value);
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData();
+    const data = { title };
+
+    image && formData.append("files.image", image, "image");
+    formData.append("data", JSON.stringify(data));
+
+    const res = await fetch("http://localhost:1337/api/todos", {
+      method: "POST",
+      body: formData,
+    });
+    const resData = await res.json();
+    console.log("🚀 ~ handleSubmit ~ res:", resData);
   };
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <button onClick={handleClick}>FETCH</button>
+      <button onClick={handleGetImages}>GET images</button>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={onHandleTitleChange}
+        />
+        <input type="file" name="image" onChange={handleUploadedFile} />
+        <button type="submit">Submit</button>
+      </form>
     </main>
   );
 }
