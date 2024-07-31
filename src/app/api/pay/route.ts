59d05c113aeee20e5f8b8287105be050c6d5e222
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-07-31 19:39:04 +0300

    save updates

--- src/app/api/pay/route.ts
+++ src/app/api/pay/route.ts
@@ -4,7 +4,7 @@ import { redirect } from "next/navigation";
 export async function POST(req: NextRequest) {
   const data = await req.formData();
   const declineMessage = data.get("reason");
-  if (declineMessage) redirect(`/declined?reason=${declineMessage}`);
+  if (declineMessage !== "Ok") redirect(`/declined?reason=${declineMessage}`);
 
   redirect("/accepted");
 }
