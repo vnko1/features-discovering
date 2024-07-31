66a937d9b5fefb21f86a404c24cac7def541f1b5
Andrii Valenko <andreyvalenko@gmail.com>
2024-07-31 16:44:20 +0300

    save

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
+// import { redirect } from "next/navigation";
 import crypto from "crypto";
 
 const merchantSecretKey = "flk3409refn54t54t*FNJRET";
@@ -6,4 +7,20 @@ export const createSignature = async (dataString: string) => {
   const hmac = crypto.createHmac("md5", merchantSecretKey);
   hmac.update(dataString, "utf8");
   return hmac.digest("hex");
+};
+
+export const pay = async (formData: FormData) => {
+  "use server";
+
+  const res = await fetch("https://secure.wayforpay.com/pay", {
+    method: "POST",
+    body: formData,
+    headers: {
+      "Accept-Charset": "utf-8",
+    },
+  });
+  if (!res.ok) console.log("🚀 ~ pay ~ res:", res);
+
+  console.log(res);
+  // redirect(res.url);
 };
