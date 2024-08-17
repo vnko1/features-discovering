87423046fdac4ef2d18880deb2dd09dc5265612b
Andrii Valenko <andreyvalenko@gmail.com>
2024-08-17 17:15:59 +0300

    save updates

--- src/app/customVideo/page.tsx
+++ src/app/customVideo/page.tsx
@@ -1,23 +1,42 @@
+"use client";
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-async function Page() {
-  const res = await axios(
-    "http://localhost:1337/api/upload/files?filters[name][$eqi]=безкоштовний урок"
-  );
+function Page() {
+  const [url, setUrl] = useState<string | null>(null);
+  const playerRef = useRef<HTMLVideoElement | null>(null);
+
+  const handlePlay = () => {
+    if (playerRef.current) playerRef.current.play();
+  };
+
+  const handlePause = () => {
+    if (playerRef.current) playerRef.current.pause();
+  };
+
+  useEffect(() => {
+    axios(
+      "http://localhost:1337/api/upload/files?filters[name][$eqi]=безкоштовний урок"
+    ).then((res) => setUrl(res.data[0].url));
+  }, []);
 
   return (
     <main className="flex h-screen items-center justify-between p-24 bg-white">
-      <div className="w-[680px] h-[420px] mx-auto ">
-        <video
-          src={res.data[0].url}
-          controls
-          className="rounded-lg"
-          //   loop
-          //   autoPlay
-          //   poster={res.data[0].previewUrl}
-        />
-      </div>
+      {url && (
+        <div className="w-[680px] h-[420px] mx-auto ">
+          <video
+            src={url}
+            // controls
+            className="rounded-lg"
+            //   loop
+            //   autoPlay
+            //   poster={res.data[0].previewUrl}
+          />
+        </div>
+      )}
+
+      <button onClick={handlePlay}>Play</button>
+      <button onClick={handlePause}>Pause</button>
     </main>
   );
 }
