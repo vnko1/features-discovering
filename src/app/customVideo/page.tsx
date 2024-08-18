5e17585b73f7749a5a5c30000450d0b37dfa7dc7
Andrii Valenko <andreyvalenko@gmail.com>
2024-08-18 08:44:31 +0300

    save updates

--- src/app/customVideo/page.tsx
+++ src/app/customVideo/page.tsx
@@ -4,14 +4,21 @@ import React, { useEffect, useRef, useState } from "react";
 
 function Page() {
   const [url, setUrl] = useState<string | null>(null);
+  const [showControl, setShowControl] = useState(false);
   const playerRef = useRef<HTMLVideoElement | null>(null);
 
   const handlePlay = () => {
-    if (playerRef.current) playerRef.current.play();
+    if (playerRef.current) {
+      playerRef.current.play();
+      setShowControl(true);
+    }
   };
 
   const handlePause = () => {
-    if (playerRef.current) playerRef.current.pause();
+    if (playerRef.current) {
+      playerRef.current.pause();
+      setShowControl(false);
+    }
   };
 
   useEffect(() => {
@@ -21,13 +28,16 @@ function Page() {
   }, []);
 
   return (
-    <main className="flex h-screen items-center justify-between p-24 bg-white">
+    <main className="flex h-screen items-center justify-between p-24 ">
       {url && (
         <div className="w-[680px] h-[420px] mx-auto ">
           <video
+            ref={playerRef}
             src={url}
-            // controls
+            controls={showControl}
             className="rounded-lg"
+            width={400}
+            height="200px"
             //   loop
             //   autoPlay
             //   poster={res.data[0].previewUrl}
--- src/app/reactPlayer/page.tsx
+++ src/app/reactPlayer/page.tsx
@@ -1,38 +1,52 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import ReactPlayer from "react-player";
 import axios from "axios";
 
-function Page() {
-  const [url, setUrl] = useState<string | null>(null);
-  const videoRef = useRef<ReactPlayer | null>(null);
+type Video = { url: string; previewUrl: string; [key: string]: string };
 
-  const handlePlay = () => {
-    if (videoRef.current) {
-      videoRef.current.getInternalPlayer()?.play();
-    }
-  };
+function Page() {
+  const [video, setVideo] = useState<null | Video>(null);
+
   useEffect(() => {
     axios(
       "http://localhost:1337/api/upload/files?filters[name][$eqi]=безкоштовний урок"
-    ).then((res) => setUrl(res.data[0].url));
+    ).then((res) => {
+      setVideo(res.data[0]);
+    });
   }, []);
+
   return (
     <main className="flex h-screen items-center justify-between p-24 ">
-      {url ? (
+      {video ? (
         <ReactPlayer
-          style={{ borderRadius: "50px" }}
-          ref={videoRef}
-          url={url}
-          //   wrapper={}
+          style={{ position: "relative" }}
+          url={video.url}
           width="400px"
-          height="400px"
-          light={false}
+          height="750px"
+          light={video.previewUrl}
+          controls
+          playIcon={<button className="absolute bottom-0 right-0">PLAY</button>}
+          previewTabIndex={1}
         />
       ) : null}
-      <button onClick={handlePlay}>PLAY</button>
     </main>
   );
 }
 
 export default Page;
+// const videoRef = useRef<ReactPlayer | null>(null);
+
+// const handlePlay = () => {
+//   if (videoRef.current) {
+//     videoRef.current.getInternalPlayer()?.play();
+//   }
+// };
+
+// const handlePause = () => {
+//   if (videoRef.current) {
+//     videoRef.current.getInternalPlayer()?.pause();
+//   }
+// };
+/* <button onClick={handlePlay}>PLAY</button>
+      <button onClick={handlePause}>PAUSE</button> */
