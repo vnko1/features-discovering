ab7db43ab6281ca9317729ef517f122bd91f9a39
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-08-23 10:37:29 +0300

    save updates

--- src/app/count/layout.tsx
+++ src/app/count/layout.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 import StoreProvider from "./StoreProvider";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  return <StoreProvider>{children}</StoreProvider>;
+  return <StoreProvider initialValue={1000}>{children}</StoreProvider>;
 };
 
 export default Layout;
