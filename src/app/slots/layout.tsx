661783d874156574cc2f871fe6b2c474c39afaa1
Andrii Valenko <andreyvalenko@gmail.com>
2024-08-27 15:52:17 +0300

    save updates

--- src/app/slots/layout.tsx
+++ src/app/slots/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./page.module.scss";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 function Layout({
   children,
@@ -10,7 +12,11 @@ function Layout({
 }) {
   return (
     <div className={styles.body}>
-      <header>Header</header>
+      <header>
+        <Link href={"/"} className={buttonVariants({ variant: "outline" })}>
+          Home
+        </Link>
+      </header>
       <main className="flex flex-col items-center">
         {tabs}
         <div className={styles.main}> {children}</div>
