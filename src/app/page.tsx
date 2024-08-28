e63c8cd782d4f67103c4d12e5a8dd9e955f63a9d
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-08-28 23:00:39 +0300

    save updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex gap-3">
+      <div className="flex gap-3 flex-wrap">
         <Link href={"/fetch"}>FETCH page</Link>
         <Link href={"/pay"}>PAY page</Link>
         <Link href={"/markdown"}>Markdown</Link>
