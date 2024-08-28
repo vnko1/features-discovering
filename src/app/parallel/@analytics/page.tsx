01a35c437218292962acf506d8a2d4d9ed093255
Andrii Valenko <andreyvalenko@gmail.com>
2024-08-28 11:18:00 +0300

    save updates

--- src/app/parallel/@analytics/page.tsx
+++ src/app/parallel/@analytics/page.tsx
@@ -1,7 +1,7 @@
 import { pause } from "@/lib/utils";
 
 export default async function Analytics() {
-  await pause(5000);
+  await pause(1000);
 
   return (
     <section className="h-96 w-96 border-2 rounded-lg p-4">
