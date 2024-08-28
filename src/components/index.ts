48bea65eb917e447e63df6e318c246c395bc29da
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-08-28 06:50:00 +0300

    save updates

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,4 @@
 export { default as Tab } from "./Tab/Tab";
+export { AlertConfirmation } from "./AlertConfirmations/AlertConfirmations";
+export { Modal } from "./Modal/Modal";
+export { InputWithLabel } from "./InputWithLabel/InputWithLabel";
