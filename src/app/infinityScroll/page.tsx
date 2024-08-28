2eb3f70254f0dd9510a59c05c71f9b7da5aa7768
Andrii Valenko <andreyvalenko@gmail.com>
2024-08-28 13:14:57 +0300

    save updates

--- src/app/infinityScroll/page.tsx
+++ src/app/infinityScroll/page.tsx
@@ -1,7 +1,116 @@
+"use client";
 import React from "react";
+import styles from "./page.module.scss";
 
 function Page() {
-  return <div>Page</div>;
+  return (
+    <section className="h-screen p-10 ">
+      <div className="w-full">
+        <div
+          className={styles.slider}
+          // data-reverse
+          style={{
+            "--width": " 100px",
+            "--height": "50px",
+            "--qty": 10,
+          }}
+        >
+          <ul className={styles.list}>
+            <li className={styles.item} style={{ "--position": 1 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+            <li className={styles.item} style={{ "--position": 2 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+            <li className={styles.item} style={{ "--position": 3 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+            <li className={styles.item} style={{ "--position": 4 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+            <li className={styles.item} style={{ "--position": 5 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+            <li className={styles.item} style={{ "--position": 6 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+            <li className={styles.item} style={{ "--position": 7 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+            <li className={styles.item} style={{ "--position": 8 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+            <li className={styles.item} style={{ "--position": 9 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+            <li className={styles.item} style={{ "--position": 10 }}>
+              <button
+                onClick={() => {
+                  console.log("click");
+                }}
+              >
+                ITEM
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Page;
