5da953a37fd0216f0a5ceb1e27c9d395bfd35a2b
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2024-08-29 06:55:47 +0300

    save updates

--- src/app/responsive/page.tsx
+++ src/app/responsive/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import styles from "./page.module.scss";
 function Page() {
-  return <div>Page</div>;
+  return (
+    <div className="p-10">
+      <div className={styles.box}></div>
+    </div>
+  );
 }
 
 export default Page;
