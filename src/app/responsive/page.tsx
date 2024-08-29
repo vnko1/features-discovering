b4b11c522260ac8bd37ccef0ceaa8c43c7afcdab
Andrii Valenko <andreyvalenko@gmail.com>
2024-08-29 09:42:10 +0300

    save updates

--- src/app/responsive/page.tsx
+++ src/app/responsive/page.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import styles from "./page.module.scss";
+// import styles from "./page.module.scss";
+import Image from "next/image";
 function Page() {
   return (
-    <div className="p-10">
-      <div className={styles.box}></div>
-    </div>
+    <section className="grid gap-8 md:grid-cols-2 md:items-center md:text-left sm:max-xl:bg-blue-50">
+      <div>
+        <Image
+          src="/americano1.jpg"
+          alt="w"
+          width={400}
+          height={400}
+          className="w-full rounded-lg"
+        />
+      </div>
+      <div>
+        <h1 className="mb-2 text-4xl font-medium max-sm:text-center">
+          Headline
+        </h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis ab
+          facere, quas non totam esse fugiat, sed ut suscipit error architecto
+          ipsam deserunt corrupti at voluptatum maiores? Deserunt, consequuntur
+          non?
+        </p>
+      </div>
+    </section>
   );
 }
 
